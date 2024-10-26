f77fbebe07f462e852543ec422f59ece1eddcf20
thevoxium <thevoxium@gmail.com>
2024-10-26 19:41:19 +0530

    Adding config files

---
+++ colors.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# Gruvbox Dark (Hard) Color Palette
+#
+# export COLOR_BACKGROUND=0xe01d2021
+# export COLOR_FOREGROUND=0xe0fbf1c7
+#
+# export COLOR_ACCENT=0xe0d65d0e
+# export COLOR_ACCENT_BRIGHT=0xe0fe8019
+#
+# export COLOR_BLACK=0xe0282828
+# export COLOR_RED=0xe0cc241d
+# export COLOR_GREEN=0xe098971a
+# export COLOR_YELLOW=0xe0d79921
+# export COLOR_BLUE=0xe0458588
+# export COLOR_MAGENTA=0xe0b16286
+# export COLOR_CYAN=0xe0689d6a
+# export COLOR_WHITE=0xe0a89984
+#
+# export COLOR_BLACK_BRIGHT=0xe0928374
+# export COLOR_RED_BRIGHT=0xe0fb4934
+# export COLOR_GREEN_BRIGHT=0xe0b8bb26
+# export COLOR_YELLOW_BRIGHT=0xe0fabd2f
+# export COLOR_BLUE_BRIGHT=0xe083a598
+# export COLOR_MAGENTA_BRIGHT=0xe0d3869b
+# export COLOR_CYAN_BRIGHT=0xe08ec07c
+# export COLOR_WHITE_BRIGHT=0xe0ebdbb2
+#
+#
+
+
+# Base Colors
+export COLOR_BACKGROUND=0xe0171a1f    # Deep blue-grey background
+export COLOR_FOREGROUND=0xe0eef1f3    # Soft white foreground
+export COLOR_ACCENT=0xe0ff6b6b        # Coral accent
+export COLOR_ACCENT_BRIGHT=0xe0ff8f8f # Bright coral
+
+# Dark Colors
+export COLOR_BLACK=0xe01e2127         # Darker blue-grey
+export COLOR_RED=0xe0e64553          # Deep red
+export COLOR_GREEN=0xe040a02b        # Forest green
+export COLOR_YELLOW=0xe0d4a93e       # Golden yellow
+export COLOR_BLUE=0xe0306893         # Navy blue
+export COLOR_MAGENTA=0xe0a15ea7      # Rich purple
+export COLOR_CYAN=0xe02b8a7c         # Deep teal
+export COLOR_WHITE=0xe0c8ccd0        # Light grey
+
+# Bright Colors
+export COLOR_BLACK_BRIGHT=0xe0384148  # Medium grey
+export COLOR_RED_BRIGHT=0xe0ff6b6b    # Bright red
+export COLOR_GREEN_BRIGHT=0xe054d62b  # Bright green
+export COLOR_YELLOW_BRIGHT=0xe0ffd75c # Bright yellow
+export COLOR_BLUE_BRIGHT=0xe04b9cd6   # Bright blue
+export COLOR_MAGENTA_BRIGHT=0xe0cf74df # Bright magenta
+export COLOR_CYAN_BRIGHT=0xe033c5ba   # Bright cyan
+export COLOR_WHITE_BRIGHT=0xe0ffffff  # Pure white
