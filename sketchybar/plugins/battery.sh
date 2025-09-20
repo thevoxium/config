ec2199e0a36920662ba487f3ff401711e7ddc646
thevoxium <thevoxium@gmail.com>
2025-09-20 07:55:45 +0530

    update

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -1,41 +1,33 @@
-# Battery is here bcause the ICON_COLOR doesn't play well with all background colors
+#!/bin/bash
 
-PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
-CHARGING=$(pmset -g batt | grep 'AC Power')
+source "$CONFIG_DIR/icons.sh"
+source "$CONFIG_DIR/colors.sh"
+
+BATTERY_INFO="$(pmset -g batt)"
+PERCENTAGE=$(echo "$BATTERY_INFO" | grep -Eo "\d+%" | cut -d% -f1)
+CHARGING=$(echo "$BATTERY_INFO" | grep 'AC Power')
 
 if [ $PERCENTAGE = "" ]; then
   exit 0
 fi
 
+DRAWING=on
+COLOR=$WHITE
 case ${PERCENTAGE} in
-  [8-9][0-9] | 100)
-    ICON=""
-    ICON_COLOR=0xFFFEFEFE
-    ;;
-  [6-7][0-9])
-    ICON=""
-    ICON_COLOR=0xFFFEFEFE
-    ;;
-  [3-5][0-9])
-    ICON=""
-    ICON_COLOR=0xFFFEFEFE
-    ;;
-  [1-2][0-9])
-    ICON=""
-    ICON_COLOR=0xfff5a97f
-    ;;
-  [0-9])
-    ICON=""
-    ICON_COLOR=0xffed8796
-    ;;
+  9[0-9]|100) ICON=$BATTERY_100; DRAWING=off
+  ;;
+  [6-8][0-9]) ICON=$BATTERY_75; DRAWING=off
+  ;;
+  [3-5][0-9]) ICON=$BATTERY_50
+  ;;
+  [1-2][0-9]) ICON=$BATTERY_25; COLOR=$ORANGE
+  ;;
+  *) ICON=$BATTERY_0; COLOR=$RED
 esac
 
 if [[ $CHARGING != "" ]]; then
-  ICON=""
-  ICON_COLOR=0xff99ff66
+  ICON=$BATTERY_CHARGING
+  DRAWING=off
 fi
-
-sketchybar --set $NAME \
-  icon=$ICON \
-  label="${PERCENTAGE}%" \
-  icon.color=${ICON_COLOR}
+DRAWING=on
+sketchybar --set $NAME drawing=$DRAWING icon="$ICON" icon.color=$COLOR
--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -1,5 +1,28 @@
-SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk -F:  '($1 ~ "^ *SSID$"){print $2}' | cut -c 2-)
+#!/bin/bash
 
-sketchybar --set $NAME \
-  icon= icon.color=0xFFFEFEFE \
-  label="$SSID"
+update() {
+  source "$CONFIG_DIR/icons.sh"
+  INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: '  '/ SSID: / {print $2}')"
+  LABEL="$INFO ($(ipconfig getifaddr en0))"
+  ICON="$([ -n "$INFO" ] && echo "$WIFI_CONNECTED" || echo "$WIFI_DISCONNECTED")"
+
+  sketchybar --set $NAME icon="$ICON" label="$LABEL"
+}
+
+click() {
+  CURRENT_WIDTH="$(sketchybar --query $NAME | jq -r .label.width)"
+
+  WIDTH=0
+  if [ "$CURRENT_WIDTH" -eq "0" ]; then
+    WIDTH=dynamic
+  fi
+
+  sketchybar --animate sin 20 --set $NAME label.width="$WIDTH"
+}
+
+case "$SENDER" in
+  "wifi_change") update
+  ;;
+  "mouse.clicked") click
+  ;;
+esac
